5dd4778f882a9792e840042cb47952b62b1e8bc5
Sam-bot-dev <bavesh2006kumar@gmail.com>
2026-03-14 15:41:36 +0530

    updated main file
    updated main file and signin/signup page

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def sign_in():
     return render_template('signin.html')
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 @app.route('/dashboard')
 def dashboard():
     return render_template('main.html')
