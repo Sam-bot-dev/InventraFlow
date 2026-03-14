5fc6f668c32cb0285e273c985c46c3b6997b9346
Sam-bot-dev <bavesh2006kumar@gmail.com>
2026-03-14 14:45:55 +0530

    added main dashboard file

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 # this is a flask app
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def sign_in():
+    return render_template('signin.html')
+@app.route('/dashboard')
+def dashboard():
+    return render_template('main.html')
+if __name__ == "__main__":
+    app.run(debug=True)
